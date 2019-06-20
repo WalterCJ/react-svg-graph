6920593b5e9b20302386f955efbe24f1fa8f1d0d
WalterCJ <waltercavalcantejunior@gmail.com>
2019-06-20 17:59:22 -0300

    add graph

--- src/components/GraphBody.js
+++ src/components/GraphBody.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
-const GraphBody = () => {
-    
+const GraphBody = (props) => {
+
     return (
-        <div>
-            
-        </div>
+        <path d={prepareData(props)}
+            stroke="orange"
+            strokeWidth={1}
+            fill="none"
+        />
     )
 }
 
+function prepareData(props) {
+    let d = [`M ${props.x} ${props.y}`];
+
+    let collector = props.data.map(chunk => {
+        let xNext = props.x + chunk[0] * props.multiplier;
+        let yNext = props.y - chunk[1] * props.multiplier;
+        return `L ${xNext} ${yNext}`;
+    });
+
+    return d.concat(collector).join(' ');
+}
+
+GraphBody.defaultProps = {
+    multiplier: 20
+};
 
 export default GraphBody
